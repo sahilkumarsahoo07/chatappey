76b3b0d14df83b007306f8bab221be5d3df2dcbe
Sahil Kumar Sahoo <sahilkumarsahoo001@gmail.com>
2025-06-01 00:39:14 +0530

    cors error

--- backend/src/index.js
+++ backend/src/index.js
@@ -18,7 +18,11 @@ app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 app.use(cookieParser());
 app.use(cors({
-    origin: "http://localhost:5173",
+    // origin: "http://localhost:5173",
+     origin: [
+    "http://localhost:5173",
+    "https://chatappey.netlify.app" 
+  ],
     credentials: true,
 }));
 
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,16 @@
+// import axios from "axios";
+
+// export const axiosInstance = axios.create({
+//     baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
+//     withCredentials: true,
+// });
+
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
-    withCredentials: true,
+  baseURL:
+    import.meta.env.MODE === "development"
+      ? "http://localhost:5001/api"
+      : "https://chatappey.onrender.com/api",
+  withCredentials: true,
 });
