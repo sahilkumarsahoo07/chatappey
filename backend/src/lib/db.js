85106d6cc0a009438e8e7415ea9bb35301a256f7
Sahil Kumar Sahoo <sahilkumarsahoo001@gmail.com>
2025-12-24 15:48:31 +0530

    added file

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -2,7 +2,11 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGODB_URL);
+        const mongoUrl = process.env.MONGODB_URL || process.env.MONGODB_URI;
+        if (!mongoUrl) {
+            throw new Error("MongoDB connection string is missing! Check MONGODB_URL or MONGODB_URI in your environment variables.");
+        }
+        const conn = await mongoose.connect(mongoUrl);
         console.log(`MongoDB connected: ${conn.connection.host}`)
     } catch (error) {
         console.log("MongoDB connected error:", error)
